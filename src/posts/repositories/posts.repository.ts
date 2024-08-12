3b7355132a1f151521777ceed7c04d31b71c4ca4
NDanczuk <nicolasdanc@gmail.com>
2024-08-12 10:52:11 -0300

    Post repository

---
+++ src/posts/repositories/posts.repository.ts
@@ -0,0 +1,43 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { PostEntity } from '../entities/post.entity';
+import { UpdatePostDto } from '../dto/update-post.dto';
+
+export class PostsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDTO: CreatePostDto): Promise<PostEntity> {
+    return this.prisma.post.create({
+      data: createPostDTO,
+    });
+  }
+
+  async findAll(): Promise<PostEntity[]> {
+    return await this.prisma.post.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updatePostDTO: UpdatePostDto): Promise<PostEntity> {
+    return this.prisma.post.update({
+      where: {
+        id,
+      },
+      data: updatePostDTO,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
