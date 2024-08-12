af4afb03e34e7c430724c7cecf34817a2df46260
NDanczuk <nicolasdanc@gmail.com>
2024-08-12 09:35:28 -0300

    Not found interceptor added

---
+++ src/common/errors/interceptors/notfound.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  NotFoundException,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { NotFountError } from '../types/NotFoundError';
+@Injectable()
+export class NotFoundInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (error instanceof NotFountError) {
+          throw new NotFoundException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
---
+++ src/common/errors/types/NotFoundError.ts
@@ -0,0 +1,1 @@
+export class NotFountError extends Error {}
